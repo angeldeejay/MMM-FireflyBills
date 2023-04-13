1adcbcc7edfbde9a74ff23a9ade38a73c9e38a05
Andres Vanegas <ukab72106@gmail.com>
2023-04-12 19:56:01 -0500

    Chaining promise all

--- MMM-FireflyBills.js
+++ MMM-FireflyBills.js
@@ -142,14 +142,14 @@ Module.register("MMM-FireflyBills", {
   },
 
   getScripts() {
-    return [this.file("node_modules/moment/dist/moment.js")];
+    return ["moment.js"];
   },
 
   // Load stylesheets
   getStyles() {
     return [
       "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
-      this.file(`./${this.name}.css`)
+      `${this.name}.css`
     ];
   }
 });
